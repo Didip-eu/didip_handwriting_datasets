8a8b0a546f6493056c6b3b301441d9a5157bf0e4
Nicolas Renet <nprenet@gmail.com>
2023-11-02 14:14:25 +0100

    Python version requirement.

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
         name="handwriting_datasets",
+        python_requires='>=3.8',
         version="0.1",
         author="Nicolas Renet",
         author_email="nprenet@gmail.com",
         description="Torch dataset classes",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        packages=["handwriting_datasets"]
+        packages=["handwriting_datasets"],
 )
