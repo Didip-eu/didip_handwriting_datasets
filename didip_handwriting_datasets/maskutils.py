d8b1b052db3e7cf1b0a80b0c8855ea7de612d155
Nicolas Renet <nprenet@gmail.com>
2025-02-04 21:07:28 +0100

    Additional function for on-disk channel.

--- didip_handwriting_datasets/maskutils.py
+++ didip_handwriting_datasets/maskutils.py
@@ -27,19 +27,17 @@ def bbox_alpha_mask(img_hwc: np.ndarray, mask_hw: np.ndarray, transparency=.4) -
     mask = (np.full( mask_hw.shape, transparency ) * np.logical_not( mask_hw) + mask_hw)
     return (mask*255).astype('uint8')
 
-def bbox_blurry_channel(img_hwc: np.ndarray, mask_hw: np.ndarray=None, kernel_size=10) -> np.ndarray:
-    """ Create a mean-filtered version of the entire BB.
-    Meant to be added to a tensor representing polygon+background image.
+def bbox_blurry_channel(img_hwc: np.ndarray, ignored=None, kernel_size=10) -> np.ndarray:
+    """ Create a mean-filtered version of the img.
 
     Args:
         img_hwc (np.ndarray): array (H,W,C)-uint8, as created from a PIL image.
-        mask_hw (np.ndarray): ignored here, but required by the interface.
+        ignored (np.ndarray): ignored here, but required by the interface.
     Output:
         np.ndarray: (H,W) channel, with int values in the range 0-255.
     """
     # note: skimage functions typically return 0-1 floats, no matter the input
     return ski.filters.rank.mean( ski.util.img_as_ubyte(ski.color.rgb2gray(img_hwc)), np.full((kernel_size,kernel_size),1))
-
 
 def bbox_blurry_channel_file_from_img(img_file_path:str, kernel_size=10, suffix=".mask.npy", compress=True) -> None:
     img_file_path = Path( img_file_path)
@@ -53,17 +51,30 @@ def bbox_blurry_channel_file_from_img(img_file_path:str, kernel_size=10, suffix=
         np.save( img_file_path.with_suffix('').with_suffix(suffix), channel_hw)
 
 
-def bbox_gray_mask(img_hwc: np.ndarray, mask_hw: np.ndarray) -> np.ndarray:
-    """ Create a gray version of the entire BB.
-    Meant to be added to a tensor representing polygon+background image.
+def bbox_gray_channel(img_hwc: np.ndarray, ignored=None) -> np.ndarray:
+    """ Create a gray version of the img.
     
     Args:
         img_hwc (np.ndarray): array (H,W,C)-uint8, as created from a PIL image.
-        mask_hw (np.ndarray): ignored here, but required by the interface.
+        ignored (np.ndarray): unused, but required by the interface.
     Output:
         np.ndarray: (H,W) channel, with int values in the range 0-255.
     """
     return ski.util.img_as_ubyte( ski.color.rgb2gray(img_hwc))
+
+
+def bbox_gray_channel_file_from_img(img_file_path:str, kernel_size=10, suffix=".mask.npy", compress=True) -> None:
+    img_file_path = Path( img_file_path)
+    img_hwc = ski.io.imread( img_file_path )
+    channel_hw = bbox_gray_channel( img_hwc )
+    if compress:
+        suffix += '.gz'
+        with gzip.GzipFile( img_file_path.with_suffix('').with_suffix(suffix), 'w') as zf:
+            np.save( zf, channel_hw)
+    else:
+        np.save( img_file_path.with_suffix('').with_suffix(suffix), channel_hw)
+
+
 
 def show_img_three_plus_one(img_chw):
     """ Combine first 3 channels with the 4th one 
